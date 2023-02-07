cf146a5815ca9bb1d11d7ee5aca1170dbefc44dc
KatyEkat <113355751+KatyEkat@users.noreply.github.com>
2023-02-07 17:48:44 +0300

    MyProfilePage

---
+++ src/pages/MyProfile/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const Myprofile = styled.section`
+    display: flex;
+    flex-direction: column;
+    margin-left: 140px;
+    margin-right: 140px;
+`
+
+export const MyprofileHeader = styled.section`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-top: 36px;
+    margin-bottom: 75px;
+`
+
+export const MyprofileCards = styled.section`
+    display: flex;
+    flex-direction: row;
+    gap: 40px;
+    
+`
+export const MyprofileBlock = styled.section`
+    display: flex;
+    flex-direction: column;
+    width: 304px;
+    height: 300px;
+`
