2014080c7304bd022168620391102dcb39858a42
michael-leonov <michael.leonov96@mail.ru>
2023-02-10 16:53:45 +0300

    fix global connection of fonts

--- src/fonts/styledParagraph16px.jsx
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components'
-
-export const StyledParagraph16px = styled.p`
-    font-size: 16px;
-`
--- src/fonts/styledParagraph32px.jsx
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components'
-
-export const StyledParagraph32px = styled.p`
-    font-size: 32px;
-`
