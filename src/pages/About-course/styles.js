40726e1d844217807bf5e2e71424fc3c23aea859
michael-leonov <michael.leonov96@mail.ru>
2023-02-14 12:37:11 +0300

    completed the styling of the page about the course

--- src/pages/About-course/styles.js
+++ src/pages/About-course/styles.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
+import { mainButton } from '../../components/Button/styles'
 
 export const Main = styled.main`
   display: flex;
   flex-direction: column;
   row-gap: 75px;
+  padding-bottom: 95px;
 `
 
 export const TitleWrapper = styled.div`
@@ -14,11 +16,26 @@ export const TitleWrapper = styled.div`
   border-radius: 30px;
   padding: 30px;
 `
+
+export const Title = styled.h1`
+  font-size: 64px;
+  font-weight: 400;
+`
+
+export const SectionTitle = styled.h2`
+  font-size: 40px;
+  font-weight: 400;
+`
+
 export const FitForYouWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(3, minmax(0, 1fr));
   justify-content: space-between;
   column-gap: 87px;
+`
+
+export const FitTitle = styled(SectionTitle)`
+  margin-bottom: 40px;
 `
 
 export const FitForYouItem = styled.div`
@@ -36,7 +53,12 @@ export const FitForYouItemIndex = styled.div`
   margin-right: 24px;
 `
 export const FitForYouItemText = styled.p`
+  font-size: 24px;
   max-width: 244px;
+`
+
+export const DirectionTitle = styled(SectionTitle)`
+  margin-bottom: 22px;
 `
 
 export const DirectionsList = styled.ul`
@@ -48,20 +70,32 @@ export const DirectionsList = styled.ul`
 `
 
 export const DirectionItem = styled.li`
+  font-size: 24px;
   margin-left: 10px;
 `
 
+export const WorkoutDescription = styled.p`
+  font-size: 24px;
+`
+
 export const TrialWorkoutBlock = styled.div`
-  padding: 46px 0 0 52px;
+  padding: 46px 285px 48px 52px;
   background: #f9ebff;
   border-radius: 30px;
-  display: flex;
-  justify-content: space-between;
-  column-gap: 20px;
+  position: relative;
 `
 
 export const TrialWorkoutDescription = styled.p`
   font-size: 32px;
   line-height: 40px;
   margin-bottom: 34px;
+`
+export const TrialButton = styled(mainButton)`
+  font-size: 18px;
+`
+
+export const TrialPhoneImg = styled.img`
+  position: absolute;
+  right: 0;
+  top: 0;
 `
